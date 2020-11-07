a0ef6abaa5e5f900e8ed61aaf88ead6d7adaadb2
Jingmin Xia <jingmin.xia@maths.ox.ac.uk>
2020-11-07 19:26:04 +0800

    Update constraint error

--- oneconstant/error.py
+++ oneconstant/error.py
@@ -1,10 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = [10, 50, 1e2, 5e2, 1e3, 5e3, 1e4, 2e4]
-y = [6.865e-9, 2.981e-9, 1.732e-9, 1.467e-9, 1.231e-9, 3.458e-10, 2.221e-10, 8.231e-11]
-
-z = 1e-3*np.power(x, -0.5) 
+x = [10, 50, 1e2,5e2, 3e3, 7e3]
+y = [7.51361757188125e-09,2.67281247230815e-09,2.05168040289809e-09,1.41534288487427e-09,3.01583497074702e-10, 7.54092160083207e-11]
+z = 1e-2*np.power(x, -0.5)
 plt.xlabel(r"$\gamma$")
 plt.ylabel(r"$||\mathbf{n}\cdot\mathbf{n}-1||_0$")
 plt.loglog(x,y, "b", marker="s", markersize=10, linewidth=1.0)
